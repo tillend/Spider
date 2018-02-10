14872247eb089a619e7553750bd8a251d6ca3cae
Lin <weihaolin2008@qq.com>
2018-02-11 00:10:41 +0800

    已完结番剧爬取

---
+++ src/test/java/com/lin/test/BiliBili_anime_Test.java
@@ -0,0 +1,29 @@
+package com.lin.test;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.lin.service.BiliBili_anime;
+
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.FilePipeline;
+
+public class BiliBili_anime_Test {
+
+	@Ignore
+	@Test
+	public void test4process() {
+		Spider.create(new BiliBili_anime())
+				.addUrl("https://www.bilibili.com/v/anime/finish/#/all/scores/0/1/2017-12-01,2018-02-08")
+				.addPipeline(new FilePipeline("/data")).thread(1).run();
+	}
+
+	@Test
+	public void test4api() {
+		Spider.create(new BiliBili_anime())
+				.addUrl("https://s.search.bilibili.com/cate/search?main_ver=v3&search_type=video&view_type=hot_rank"
+						+ "&cate_id=32&page=1&pagesize=20&jsonp=jsonp"
+						+ "&time_from=20171201&time_to=20180208")
+				.addPipeline(new FilePipeline("/data")).thread(1).run();
+	}
+}
