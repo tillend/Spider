ffa35f0b62b5fb924b58c12ae445f001927b8b67
Lin <weihaolin2008@qq.com>
2018-02-12 20:28:31 +0800

    增加Date转换工具类

--- src/main/java/com/lin/utils/DateUtil.java
+++ src/main/java/com/lin/utils/DateUtil.java
@@ -7,12 +7,18 @@ import java.util.Date;
 
 public class DateUtil {
 	
-	//默认样式
+	public static String defaultPattern =  "yyyy-MM-dd HH:mm:ss";
+	
+	/**
+	 * 模式字符串转换为java.util.Date
+	 * @param dateString
+	 * @return
+	 */
 	public static Date string2Date(String dateString) {
-		return string2Date(dateString, "yyyy-MM-dd HH:mm:ss");
+		return string2Date(dateString, defaultPattern);
 	}
 
-	private static Date string2Date(String dateString, String pattern) {
+	public static Date string2Date(String dateString, String pattern) {
 		DateFormat format = new SimpleDateFormat(pattern);     
 		
 		Date date = null;
@@ -24,6 +30,35 @@ public class DateUtil {
 		}
 		return date;
 		
+	}
+
+	/**
+	 * unixtime(毫秒数)转换为java.util.Date
+	 * @param time
+	 * @return
+	 */
+	public static Date unixtime2Date(Long time) {
+		return unixtime2Date(time, defaultPattern);
+	}
+
+	public static Date unixtime2Date(Long time, String pattern) {
+		return new Date(time);
+	}
+	
+	/**
+	 * unixtime(毫秒数)转换为时间串,eg. yyyy-MM-dd HH:mm:ss
+	 * @param time
+	 * @return
+	 */
+	public static String unixtime2FormatString(Long time) {
+		return unixtime2FormatString(time, defaultPattern);
+	}
+	
+	public static String unixtime2FormatString(Long time, String pattern) {
+		DateFormat format = new SimpleDateFormat(pattern);	
+		Date date = new Date(time);
+		
+		return format.format(date);
 	}
 
 }
